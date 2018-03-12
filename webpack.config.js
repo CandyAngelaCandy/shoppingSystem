725ad570f5d6327feca6f7030b86ef54a3ad61f1
Candy <229355694@qq.com>
2018-03-12 21:34:13 +0800

    delete node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-  /*entry: './src/index.js', //相对路径*/
-  entry:'C:/Users/22935/WebstormProjects/shoppingSystem_react/src/index.js',
+  entry: './src/index.js', //相对路径*!/
   output: {
-    path: path.resolve(__dirname, 'build'), //打包文件的输出路径
+    path: path.resolve(__dirname, 'build'), //打包文件的输出路径,__dirname是node.js中的一个全局变量，
+    // 它指向当前执行脚本所在的目录
     filename: 'bundle.js', //打包文件名
   },
 
@@ -21,6 +21,17 @@ module.exports = {
         query: { //babel的配置参数，可以写在.babelrc文件里也可以写在这里
           presets: ['env', 'react']
         }
+      },
+      {
+        test: /\.css/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: [/\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: 'url-loader',
+        options: {
+          limit: 10000, //1w字节以下大小的图片会自动转成base64
+        },
       }
     ]
   },
@@ -28,8 +39,14 @@ module.exports = {
     new HtmlWebpackPlugin({//html-webpack-plugin自动生成模板文件，以及后面用到的分离CSS等。
       template: './public/index.html', //指定模板路径
       filename: 'index.html', //指定文件名
-    })
-  ]
+    }),
+   new webpack.HotModuleReplacementPlugin()//热模块替换插件
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 8080
+  },
 }
 
 
